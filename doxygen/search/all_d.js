5a4a5aa8fd0977817562c3443d62f79a67d6dca8
lucsch <lucsch@users.noreply.github.com>
2022-12-02 14:32:29 +0000

    deploy: terranum-ch/ToolMap@c3b6f7dd04eddd957879e2440dd762417a3ffe45

--- doxygen/search/all_d.js
+++ doxygen/search/all_d.js
@@ -1,5 +1,9 @@
 var searchData=
 [
   ['newtemplateprjwizard_0',['NewTemplatePrjWizard',['../classNewTemplatePrjWizard.html',1,'']]],
-  ['num_1',['num',['../structgeod__polygon.html#a4478dc71b2bd7659afeda0ae2c84a3ac',1,'geod_polygon']]]
+  ['nodeadd_1',['NodeAdd',['../classTocCtrlModel.html#a2c1c7d274b30c6eb9ee5c4fe24f9f6a0',1,'TocCtrlModel::NodeAdd(TocCtrlModelNode *parent, const wxString &amp;branch)'],['../classTocCtrlModel.html#a5b30fba08fe4b082b99d5af7dea709cd',1,'TocCtrlModel::NodeAdd(TocCtrlModelNode *parent, tmLayerProperties *layerprop)']]],
+  ['nodegettitle_2',['NodeGetTitle',['../classTocCtrlModel.html#a49965b13da7fca2ef29d9660fd690484',1,'TocCtrlModel']]],
+  ['nodemove_3',['NodeMove',['../classTocCtrlModel.html#a545334afb812b7def2e25aa4601722c4',1,'TocCtrlModel']]],
+  ['nodesettitle_4',['NodeSetTitle',['../classTocCtrlModel.html#affdbb26abda9687a8b31ca1f045ba0c2',1,'TocCtrlModel']]],
+  ['num_5',['num',['../structgeod__polygon.html#a4478dc71b2bd7659afeda0ae2c84a3ac',1,'geod_polygon']]]
 ];
